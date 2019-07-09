927909170dbd03c09d0b226adafd6a5923155d8c
niederle <claude.niederlender@inist.fr>
2019-07-09 13:33:51 +0200

    change password variable name

--- entrypoint-overload.sh
+++ entrypoint-overload.sh
@@ -6,14 +6,14 @@ bash ./parse-config.sh
 if [[ -f "config.env" ]];
 then
     . config.env
+    echo "config.env parsed, using the folling parameters :"
     cat config.env
-    node app.js
 else
     echo "cannot read config.env file"
 fi
 
 # GENERATE SSH KEYS
-echo "root:${ROOT_PASSWORD}" | chpasswd
+echo "root:${SSHSERVER_PASSWORD}" | chpasswd
 sed -ie 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
 sed -ri 's/#HostKey \/etc\/ssh\/ssh_host_key/HostKey \/etc\/ssh\/ssh_host_key/g' /etc/ssh/sshd_config
 sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_rsa_key/HostKey \/etc\/ssh\/ssh_host_rsa_key/g' /etc/ssh/sshd_config
